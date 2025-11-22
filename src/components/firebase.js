213545bf0989f0100ca6fc0e818f4b61f0e7d6fd
Raj Singh <rajsingh76554@gmail.com>
2025-11-22 10:17:29 +0530

    updating .env

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,15 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// 👉 Yahan apna Firebase config paste karo
 const firebaseConfig = {
-  apiKey: "AIzaSyDTLIETTg-BO9fw7JqfpPfFEADPW3RSBAg",
-  authDomain: "vizzle-landingpage.firebaseapp.com",
-  projectId: "vizzle-landingpage",
-  storageBucket: "vizzle-landingpage.firebasestorage.app",
-  messagingSenderId: "1061160328748",
-  appId: "1:1061160328748:web:6497361f2c73f023e9bb89",
-  measurementId: "G-HC2Z40YG88"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Init Firebase
